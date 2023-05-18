9f913214bba34538f3645fae27188fd89015ed5c
arthurpontespiazzaroli@gmail.com <arthurpontespiazzaroli@gmail.com>
2023-05-18 18:56:30 -0300

    feat: home comeco

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,167 @@
-import React from "react"; 
- 
-function Home(){ 
-  return ( 
-    <div> 
-      <h1>Home</h1> 
-    </div> 
-  ) 
-} 
- 
-export default Home;
+import React, { useEffect, useState } from "react";
+import {
+  Body,
+  BotoesEdicao,
+  CaixaBotoes,
+  CaixaFoto,
+  CaixaInputs,
+  ConjuntoTituloInput,
+  Conteudo,
+  InputDividido,
+  TituloIcon,
+  TituloInput,
+} from "./Styles";
+
+import {
+  IdcardOutlined,
+  PhoneOutlined,
+  CalendarOutlined,
+  MailOutlined,
+  CopyOutlined,
+  GlobalOutlined,
+} from "@ant-design/icons";
+
+import Botao from "../../Styles/Botao/Botao";
+import Input from "../../Styles/Input/Input";
+import fotoPerfil from "../../assets/montanha.jpg"
+
+import * as managerService from "../../services/ManagerService/managerService";
+
+
+
+function Perfil() {
+  const [usuario, setUsuario] = useState({});
+  const id = '646614fa7e221267c351d2c5';
+
+  async function pegandoDadosUsuario() {
+    const resposta = await managerService.GetDadosUsuario(id);
+    setUsuario(resposta.dadosUsuario);
+  }
+
+  useEffect(() => {
+    pegandoDadosUsuario();
+  }, []);
+
+  return (
+    <Body>
+      <Conteudo>
+        <CaixaFoto>
+          <img
+            src={fotoPerfil}
+            width="100%"
+            height="100%"
+            style={{ borderRadius: '100%' }}
+          ></img>
+        </CaixaFoto>
+        <CaixaInputs>
+          <ConjuntoTituloInput>
+            <TituloIcon>
+              <TituloInput>NOME COMPLETO:</TituloInput>
+              <IdcardOutlined style={{ fontSize: "18px", color: "#570B87" }} />
+            </TituloIcon>
+            <Input
+              placeholder="Digite seu nome completo"
+              backgroundColor="white"
+              heightMedia700="20px"
+              marginBottomMedia700="8%"
+            ></Input>
+          </ConjuntoTituloInput>
+          <InputDividido>
+            <ConjuntoTituloInput>
+              <TituloIcon>
+                <TituloInput>TELEFONE:</TituloInput>
+                <PhoneOutlined style={{ fontSize: "18px", color: "#570B87" }} />
+              </TituloIcon>
+              <Input
+                placeholder="Digite seu número de telefone"
+                backgroundColor="white"
+                width="100%"
+                heightMedia700="20px"
+                alignSelf="flex-start"
+                marginBottomMedia700="8%"
+              ></Input>
+            </ConjuntoTituloInput>
+            <ConjuntoTituloInput>
+              <TituloIcon>
+                <TituloInput>DATA DE NASCIMENTO:</TituloInput>
+                <CalendarOutlined
+                  style={{ fontSize: "18px", color: "#570B87" }}
+                />
+              </TituloIcon>
+              <Input
+                placeholder="Digite sua data de nascimento:"
+                backgroundColor="white"
+                width="100%"
+                heightMedia700="20px"
+                alignSelf="flex-start"
+                marginBottomMedia700="8%"
+              ></Input>
+            </ConjuntoTituloInput>
+          </InputDividido>
+          <ConjuntoTituloInput>
+            <TituloIcon>
+              <TituloInput>EMAIL:</TituloInput>
+              <MailOutlined style={{ fontSize: "18px", color: "#570B87" }} />
+            </TituloIcon>
+            <Input
+              placeholder="Digite seu endereço de e-mail"
+              backgroundColor="white"
+              heightMedia700="20px"
+              marginBottomMedia700="8%"
+            ></Input>
+          </ConjuntoTituloInput>
+          <InputDividido>
+            <ConjuntoTituloInput>
+              <TituloIcon>
+                <TituloInput>CRM:</TituloInput>
+                <CopyOutlined style={{ fontSize: "18px", color: "#570B87" }} />
+              </TituloIcon>
+              <Input
+                placeholder="Digite seu CRM"
+                backgroundColor="white"
+                width="100%"
+                heightMedia700="20px"
+                alignSelf="flex-start"
+                marginBottomMedia700="8%"
+              ></Input>
+            </ConjuntoTituloInput>
+            <ConjuntoTituloInput>
+              <TituloIcon>
+                <TituloInput>UNIDADE FEDERATIVA</TituloInput>
+                <GlobalOutlined
+                  style={{ fontSize: "18px", color: "#570B87" }}
+                />
+              </TituloIcon>
+              <Input
+                placeholder="Digite a Unidade Federativa do CRM"
+                backgroundColor="white"
+                width="100%"
+                heightMedia700="20px"
+                justifyContent="flex-start"
+                alignSelf="flex-start"
+                marginBottomMedia700="8%"
+              ></Input>
+            </ConjuntoTituloInput>
+          </InputDividido>
+        </CaixaInputs>
+        <CaixaBotoes>
+          <BotoesEdicao>
+            <Botao>Alterar Dados</Botao>
+            <Botao>Alterar Senha</Botao>
+          </BotoesEdicao>
+          <Botao
+            color="#000000"
+            backgroundColor="white"
+            borderColor="#FF000080"
+            width="20%"
+            widthMedia700="30%"
+          >
+            Excluir
+          </Botao>
+        </CaixaBotoes>
+      </Conteudo>
+    </Body>
+  );
+}
+
+export default Perfil;
---
+++ src/pages/Home/Styles.js
@@ -0,0 +1,158 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Conteudo = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 90%;
+  height: 100%;
+
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+`;
+
+export const CaixaFoto = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 12em;
+  font-size: 12em;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  background-color: grey;
+  margin: 2%;
+
+  @media (max-width: 900px) {
+    width: 200px;
+    height: 200px;
+  }
+
+  @media (max-width: 500px) {
+    width: 150px;
+    height: 150px;
+  }
+`;
+
+export const CaixaInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+  align-items: center;
+  height: 70%;
+  width: 70%;
+  padding: 2%;
+
+  @media (max-width: 900px) {
+    width: 90%;
+  }
+
+  @media (max-width: 700px) {
+    gap: 5px;
+  }
+`;
+
+export const ConjuntoTituloInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 7px;
+  align-items: center;
+  height: 100%;
+  width: 70%;
+
+  @media (max-width: 700px) {
+    gap: 0px;
+  }
+`;
+
+export const InputDividido = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 50px;
+  height: 100%;
+  width: 70%;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: space-evenly;
+    gap: 5px;
+  }
+`;
+
+export const TituloInput = styled.div`
+  width: 100%;
+  height: 20px;
+  font-family: "Roboto Condensed";
+  font-weight: 400;
+  font-size: 1.2em;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  color: darkpurple;
+
+  @media (max-width: 900px) {
+    font-size: 1em;
+  }
+
+  @media (max-width: 500px) {
+    font-size: 0.8em;
+  }
+`;
+
+export const TituloIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 1%;
+  width: 100%;
+`;
+
+export const CaixaBotoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  height: 20%;
+  width: 100%;
+  padding: 2%;
+
+  @media (max-width: 900px) {
+    gap: 20px;
+    padding: 5%;
+  }
+`;
+
+export const BotoesEdicao = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 50%;
+  width: 65%;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+  }
+`;
