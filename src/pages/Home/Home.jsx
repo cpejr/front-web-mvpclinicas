b2248ecc3e1049f62c578b90b2c187e25024755e
osbarros <ceobarros@outlook.com>
2023-05-25 12:33:29 -0300

    fix: identação

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -38,7 +38,7 @@ function Home() {
     const resposta = await managerService.GetDadosLocais();
     setLocais(resposta.dadosLocais);
   }
-  
+
   useEffect(() => {
     pegandoDadosDeLocais();
   }, []);
@@ -46,79 +46,79 @@ function Home() {
   return (
     <Body>
       <Conteudo>
-      <CaixaInputs>
-        <Input
-        placeholder="Pesquisar Local"
-        backgroundColor="white"
-        width="70%"
-        borderColor="#570B87"
-        borderWidth="2px"
-        borderRadius="10px"
-        fontSize="1.5em"
-        padding="10px">
-        </Input>
-        <SearchOutlined style={{ fontSize: "28px", color: "#570B87",position:"absolute",right:"19%",paddingBottom:"1.8%"}}/>
-      </CaixaInputs>
-      <CaixaSelect>
-      <Select
-				backgroundColor={Cores.branco}
-				color="#570B87"
-				borderColor="#570B87"
-        borderWidth="2px"
-        borderRadius="10px"
-				fontSize="17px"
-				width="20%"
-				marginTop="0px"
-				placeholder="Pesquisar por nome"
-				height="45px"
-				nome="id_usuario"
-        borderWidth820="100%"
-			>
-				<option value="">
-          Pesquisar por nome
-				</option>	
-			</Select>
-      </CaixaSelect>
-      {locais.length === 0 ? (
-        <CaixaPlaceholder>
-        <TextoPlaceholder>Ainda não existem Locais Cadastrados</TextoPlaceholder>
-        </CaixaPlaceholder>
-      ) : (
-      <CaixaLocais>
-      {locais?.map((value, index) => (
-								<Local key={index}>
-                  <CaixaFoto>
+        <CaixaInputs>
+          <Input
+            placeholder="Pesquisar Local"
+            backgroundColor="white"
+            width="70%"
+            borderColor="#570B87"
+            borderWidth="2px"
+            borderRadius="10px"
+            fontSize="1.5em"
+            padding="10px">
+          </Input>
+          <SearchOutlined style={{ fontSize: "28px", color: "#570B87", position: "absolute", right: "19%", paddingBottom: "1.8%" }} />
+        </CaixaInputs>
+        <CaixaSelect>
+          <Select
+            backgroundColor={Cores.branco}
+            color="#570B87"
+            borderColor="#570B87"
+            borderWidth="2px"
+            borderRadius="10px"
+            fontSize="17px"
+            width="20%"
+            marginTop="0px"
+            placeholder="Pesquisar por nome"
+            height="45px"
+            nome="id_usuario"
+            borderWidth820="100%"
+          >
+            <option value="">
+              Pesquisar por nome
+            </option>
+          </Select>
+        </CaixaSelect>
+        {locais.length === 0 ? (
+          <CaixaPlaceholder>
+            <TextoPlaceholder>Ainda não existem Locais Cadastrados</TextoPlaceholder>
+          </CaixaPlaceholder>
+        ) : (
+          <CaixaLocais>
+            {locais?.map((value, index) => (
+              <Local key={index}>
+                <CaixaFoto>
                   <img
-                  src={value.foto_url}
-                  width="100%"
-                  height="100%"
+                    src={value.foto_url}
+                    width="100%"
+                    height="100%"
                   ></img>
-                  </CaixaFoto>
-                  <CaixaDados>
-									<NomeLocal>{value?.nome}</NomeLocal>
-									<EnderecoLocal>{value?.endereco}</EnderecoLocal>
+                </CaixaFoto>
+                <CaixaDados>
+                  <NomeLocal>{value?.nome}</NomeLocal>
+                  <EnderecoLocal>{value?.endereco}</EnderecoLocal>
                   <EstrelasLocal>
-                  {value?.estrelas}<Rate value={value?.estrelas} style={{color: "#570B87"}} disabled/>
-									</EstrelasLocal>
-                  </CaixaDados>
-								</Local>
-				))}
-      </CaixaLocais>
-      )}
-      <div className="botoes-direita" style={{width: "100%", justifyContent: "flex-end"}}>
-      <CaixaBotoes>
-      <Botao
-      borderRadius="10px"
-      width="100%"
-      alignSelf="flex-end"
-      fontSize="25px"
-      height="50px"
-      HeightMedia500="50px"
-      widthMedia500="100%"
-      >
-        Adicionar Local
-      </Botao>
-      </CaixaBotoes></div>
+                    {value?.estrelas}<Rate value={value?.estrelas} style={{ color: "#570B87" }} disabled />
+                  </EstrelasLocal>
+                </CaixaDados>
+              </Local>
+            ))}
+          </CaixaLocais>
+        )}
+        <div className="botoes-direita" style={{ width: "100%", justifyContent: "flex-end" }}>
+          <CaixaBotoes>
+            <Botao
+              borderRadius="10px"
+              width="100%"
+              alignSelf="flex-end"
+              fontSize="25px"
+              height="50px"
+              HeightMedia500="50px"
+              widthMedia500="100%"
+            >
+              Adicionar Local
+            </Botao>
+          </CaixaBotoes></div>
       </Conteudo>
     </Body>
   );
