9df4f856a67336b2bea33d93da195260b6490651
osbarros <ceobarros@outlook.com>
2023-05-25 12:57:19 -0300

    fix: correção arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import Cadastro from "../src/pages/Cadastro";
-import Home from "../src/pages/Home";
-import Local from "../src/pages/Local";
-import Login from "../src/pages/Login";
-import NovoComentario from "../src/pages/NovoComentario";
-import NovoLocal from "../src/pages/NovoLocal";
-import Perfil from "../src/pages/Perfil";
 
 import Routes from "./routes";
 
