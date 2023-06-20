e8a22667b037b19fee62c190bb6542475df5f1c0
Unknown <unknown@example.com>
2023-06-19 22:32:33 -0300

    fix: alterar dados funcional

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import useAuthStore from "../../stores/auth";
+import api from "../../services/api";
 import{
     Body,
-    //BotaoConfirme,
     CaixaBotao,
-    CaixaFoto,
     CaixaInputs,
     ConjuntoTituloInput,
     Conteudo,
@@ -26,20 +23,20 @@ import {
   
 import Botao from "../../Styles/Botao/Botao";
 import Input from "../../Styles/Input/Input";
-import fotoPerfil from "../../assets/montanha.jpg";
 
 import * as managerService from "../../services/ManagerService/managerService";
 
+
 function Edit(){
-    const usuario = useAuthStore((state) => state.usuario?._id);
-    const setUsuario = useAuthStore((state) => state.setUsuario);
-    const id = '6466a62695e98cb373b670f4'; {/* PERGUNTAR SOBRE ISSO */}
+    const [usuario, setUsuario] = useState({});
+    const id = '6466a62695e98cb373b670f4';
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log(nome);
     
         try {
-          const res = await api.put(`/usuarios/${usuario}`, {
+          const res = await api.put(`/usuarios/${id}`, {
             nome,
             telefone,
             data_nascimento,
@@ -47,6 +44,7 @@ function Edit(){
             crm,
             uni_federativa,
           });
+          window.location.href="/perfil";
           setUsuario(res.data);
         } catch (erro) {
           console.error(erro);
@@ -54,14 +52,12 @@ function Edit(){
         }
     };
 
-    const [nome, setNome] = useState(usuario.nome); //ver no backend o nome das variáveis para colocar certo
-    const [email, setEmail] = useState(usuario.email); //ver no backend o nome das variáveis para colocar certo
-    const [data_nascimento, setDataNascimento] = useState(usuario.data_nascimento); //ver no backend o nome das variáveis para colocar certo
-    const [telefone, setTelefone] = useState(usuario.telefone); //ver no backend o nome das variáveis para colocar certo
-    const [crm, setCrm] = useState(usuario.crm); //ver no backend o nome das variáveis para colocar certo
-    const [uni_federativa, setUniFederativa] = useState(usuario.uni_federativa); //ver no backend o nome das variáveis para colocar certo
-
-    const navigate = useNavigate();
+    const [nome, setNome] = useState(usuario.nome); 
+    const [email, setEmail] = useState(usuario.email); 
+    const [data_nascimento, setDataNascimento] = useState(usuario.data_nascimento); 
+    const [telefone, setTelefone] = useState(usuario.telefone); 
+    const [crm, setCrm] = useState(usuario.crm); 
+    const [uni_federativa, setUniFederativa] = useState(usuario.uni_federativa); 
 
     async function pegandoDadosUsuario() {
         const resposta = await managerService.GetDadosUsuario(id);
@@ -74,70 +70,89 @@ function Edit(){
 
     return(
         <Body>
-            <Conteudo>
-                {/* <CaixaFoto> (VER COMO EDITA FOTOS NO REACT)
-                    <img src="" alt="" />
-                </CaixaFoto> */}
-                    <Form onSubmit={handleSubmit}>
-                        <CaixaInputs>
+            <Form onSubmit={handleSubmit}>
+                <Conteudo>
+                    <CaixaInputs>
+                        <ConjuntoTituloInput>
+                            <TituloIcon>
+                                <TituloInput>Nome Completo: </TituloInput>
+                                <IdcardOutlined style={{ fontSize: "22px", color: "#570B87" }} />
+                            </TituloIcon>
+                            <Input 
+                            required 
+                            placeholder={usuario.nome} 
+                            backgroundColor="white" 
+                            onChange={(e) => setNome(e.target.value)} />
+                        </ConjuntoTituloInput>
+
+                        <InputDividido>
                             <ConjuntoTituloInput>
                                 <TituloIcon>
-                                    <TituloInput>Nome Completo: </TituloInput>
-                                    <IdcardOutlined style={{ fontSize: "22px", color: "#570B87" }} />
+                                    <TituloInput>Telefone: </TituloInput>
+                                    <PhoneOutlined style={{ fontSize: "22px", color: "#570B87" }} />
                                 </TituloIcon>
-                                <Input type="text" required placeholder={usuario.nome} backgroundColor="white" onChange={(e) => setNome(e.target.value)} />
+                                <Input 
+                                required 
+                                placeholder={usuario.telefone} 
+                                backgroundColor="white" 
+                                onChange={(e) => setTelefone(e.target.value)}/>
                             </ConjuntoTituloInput>
-
-                            <InputDividido>
-                                <ConjuntoTituloInput>
-                                    <TituloIcon>
-                                        <TituloInput>Telefone: </TituloInput>
-                                        <PhoneOutlined style={{ fontSize: "22px", color: "#570B87" }} />
-                                    </TituloIcon>
-                                    <Input type="tel" required placeholder={usuario.telefone} backgroundColor="white" onChange={(e) => setTelefone(e.target.value)}/>
-                                </ConjuntoTituloInput>
-                                <ConjuntoTituloInput>
-                                    <TituloIcon>
-                                        <TituloInput>Data de Nascimento: </TituloInput>
-                                        <CalendarOutlined style={{ fontSize: "22px", color: "#570B87" }}/>
-                                    </TituloIcon>
-                                    <Input type="date" required placeholder={usuario.data_nascimento} backgroundColor="white" onChange={(e) => setDataNascimento(e.target.value)}/>
-                                </ConjuntoTituloInput>
-                            </InputDividido>
-
                             <ConjuntoTituloInput>
                                 <TituloIcon>
-                                    <TituloInput>E-mail: </TituloInput>
-                                    <MailOutlined style={{ fontSize: "22px", color: "#570B87" }} />
+                                    <TituloInput>Data de Nascimento: </TituloInput>
+                                    <CalendarOutlined style={{ fontSize: "22px", color: "#570B87" }}/>
                                 </TituloIcon>
-                                <Input type="email" required placeholder={usuario.email} backgroundColor="white" onChange={(e) => setEmail(e.target.value)}/>
+                                <Input 
+                                required 
+                                placeholder={usuario.data_nascimento} 
+                                backgroundColor="white" 
+                                onChange={(e) => setDataNascimento(e.target.value)}/> {/* VER COMO ALTERAR FORMATO DA DATA */}
                             </ConjuntoTituloInput>
+                        </InputDividido>
 
-                            <InputDividido>
-                                <ConjuntoTituloInput>
-                                    <TituloIcon>
-                                        <TituloInput>CRM: </TituloInput>
-                                        <CopyOutlined style={{ fontSize: "22px", color: "#570B87" }} />
-                                    </TituloIcon>
-                                    <Input type="text" placeholder={usuario.crm} backgroundColor="white" onChange={(e) => setCrm(e.target.value)}/>
-                                </ConjuntoTituloInput>
-                                <ConjuntoTituloInput>
-                                    <TituloIcon>
-                                        <TituloInput>Unidade Federativa: </TituloInput>
-                                        <GlobalOutlined style={{ fontSize: "22px", color: "#570B87" }}/>
-                                    </TituloIcon>
-                                    <Input type="text" required placeholder={usuario.uni_federativa} backgroundColor="white" onChange={(e) => setUniFederativa(e.target.value)}/>
-                                </ConjuntoTituloInput>
-                            </InputDividido>
+                        <ConjuntoTituloInput>
+                            <TituloIcon>
+                                <TituloInput>E-mail: </TituloInput>
+                                <MailOutlined style={{ fontSize: "22px", color: "#570B87" }} />
+                            </TituloIcon>
+                            <Input 
+                            required 
+                            placeholder={usuario.email} 
+                            backgroundColor="white" 
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        </ConjuntoTituloInput>
 
-                        </CaixaInputs>
+                        <InputDividido>
+                            <ConjuntoTituloInput>
+                                <TituloIcon>
+                                    <TituloInput>CRM: </TituloInput>
+                                    <CopyOutlined style={{ fontSize: "22px", color: "#570B87" }} />
+                                </TituloIcon>
+                                <Input 
+                                required
+                                placeholder={usuario.crm} 
+                                backgroundColor="white" 
+                                onChange={(e) => setCrm(e.target.value)}/>
+                            </ConjuntoTituloInput>
+                            <ConjuntoTituloInput>
+                                <TituloIcon>
+                                    <TituloInput>Unidade Federativa: </TituloInput>
+                                    <GlobalOutlined style={{ fontSize: "22px", color: "#570B87" }}/>
+                                </TituloIcon>
+                                <Input 
+                                required 
+                                placeholder={usuario.uni_federativa} 
+                                backgroundColor="white" 
+                                onChange={(e) => setUniFederativa(e.target.value)}/>
+                            </ConjuntoTituloInput>
+                        </InputDividido>
 
-                        <CaixaBotao>
-                            <Botao type="submit" onClick={() => navigate("/perfil")}> Confirmar </Botao>
-                        </CaixaBotao>
-                        
-                    </Form>
-            </Conteudo>
+                    </CaixaInputs>
+                    <CaixaBotao>
+                        <Botao type="submit"> Confirmar </Botao>
+                    </CaixaBotao>
+                </Conteudo>
+            </Form>
         </Body>
     );
 }
--- src/pages/EditSenha/EditSenha.jsx
+++ src/pages/EditSenha/EditSenha.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import useAuthStore from "../../stores/auth";
 import{
     Body,
     CaixaBotao,
@@ -17,21 +15,20 @@ import Botao from "../../Styles/Botao/Botao";
 import Input from "../../Styles/Input/Input";
 
 import * as managerService from "../../services/ManagerService/managerService";
+import api from "../../services/api";
 
 function EditSenha(){
-    const navigate = useNavigate();
-
-    const usuario = useAuthStore((state) => state.usuario?._id);
-    const setUsuario = useAuthStore((state) => state.setUsuario);
-    const id = '6466a62695e98cb373b670f4'; {/* PERGUNTAR SOBRE ISSO */}
+    const [usuario, setUsuario] = useState({});
+    const id = '6466a62695e98cb373b670f4';
 
     const handleSubmit = async (e) => {
         e.preventDefault();
     
         try {
-          const res = await api.put(`/usuarios/${usuario}`, {
+          const res = await api.put(`/usuarios/${id}`, {
             senha
           });
+          window.location.href="/perfil";
           setUsuario(res.data);
         } catch (erro) {
           console.error(erro);
@@ -67,14 +64,14 @@ function EditSenha(){
                     <CaixaInputs>
                         <ConjuntoTituloInput> {/* VER SE VAI COLOCAR ÍCONE. SE FOR, TEM QUE COLOCAR O TituloInput E A TAG DE ÍCONE DENTRO DA TAG TituloIcon */}
                             <TituloInput>Nova senha: </TituloInput>
-                            <Input type="text" backgroundColor="white" onChange={(e) => setSenha(e.target.value)}/> {/* FORA DA TituloIcon */}
+                            <Input type="password" backgroundColor="white" onChange={(e) => setSenha(e.target.value)}/> {/* FORA DA TituloIcon */}
                         </ConjuntoTituloInput>
                         <ConjuntoTituloInput> {/* VER QUESTÃO DO ÍCONE */}
                             <TituloInput>Confirme a senha: </TituloInput>
-                            <Input type="text" required backgroundColor="white" onChange={(e) => setConfirmeSenha(e.target.value)}/>
+                            <Input type="password" required backgroundColor="white" onChange={(e) => setConfirmeSenha(e.target.value)}/>
                         </ConjuntoTituloInput>
                         <CaixaBotao>
-                            <Botao type="submit" onClick={() => navigate("/perfil")}> Confirmar </Botao>
+                            <Botao type="submit"> Confirmar </Botao>
                         </CaixaBotao>
                     </CaixaInputs>
                 </Form>
