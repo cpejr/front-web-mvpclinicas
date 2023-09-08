7c9af1e3ddb994da242f955f7bd7085853434a49
SamuelFVG <samuelfelipeif@gmail.com>
2023-09-08 18:40:33 -0300

    fix: correções para funcionamento correto de imagens de local

--- src/pages/Local/Local.jsx
+++ src/pages/Local/Local.jsx
@@ -147,7 +147,7 @@ function Local() {
             <img
               src="https://i0.wp.com/www.multarte.com.br/wp-content/uploads/2019/01/totalmente-transparente-png-fw.png?fit=696%2C392&ssl=1"
               style={{
-                backgroundImage: `url(http://localhost:8080/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${local.foto_url}&key=AIzaSyBUwXbN66GC9i-ZGfQmEY8n_QXGytWBe6I)`,
+                backgroundImage: `url(https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${local.foto_url}&key=AIzaSyBUwXbN66GC9i-ZGfQmEY8n_QXGytWBe6I)`,
                 borderRadius: "2%",
               }}
             />
