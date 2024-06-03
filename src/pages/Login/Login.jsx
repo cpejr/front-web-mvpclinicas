2c31f60756ccd95d214b79f4ebff12243b28232e
SamuelFVG <samuelfelipeif@gmail.com>
2024-06-03 11:12:01 -0300

    fix: remove unused code

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
-import {
-  Body,
-  BotoesEdicao,
-  CaixaBotoes,
-  CaixaFoto,
-  CaixaInput,
-  Conteudo,
-  BotaoCadastro,
-  TituloInput,
-  TituloIcon,
-  InputNovo,
-  Rotulo,
-  RotuloSenha,
-} from "./Styles";
-import Botao from "../../Styles/Botao/Botao";
-import Input from "../../Styles/Input/Input";
-import { LoadingOutlined } from "@ant-design/icons";
+import { Body, BotaoCadastro, CaixaFoto } from "./Styles";
+
 import { toast } from "react-toastify";
-import _ from "lodash";
-import { Spin } from "antd";
 import useAuthStore from "../../stores/auth";
 import { useLogin } from "../../hooks/user";
 import { useNavigate } from "react-router-dom";
@@ -28,9 +11,6 @@ import Form from "../../Components/Form";
 import { loginSchema } from "./loginSchema";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [erro, setErro] = useState(false);
   const setToken = useAuthStore((state) => state.setToken);
   const navigate = useNavigate();
 
@@ -46,70 +26,8 @@ function Login() {
     },
   });
 
-  const referenciaCamposNulos = {
-    email: false,
-    senha: false,
-  };
-  const [camposVazios, setCamposVazios] = useState({
-    email: true,
-    senha: true,
-  });
-  const [erroLoginInvalido, setErroLoginInvalido] = useState(
-    referenciaCamposNulos
-  );
-  const [deuUploadInicial, setDeuUploadInicial] = useState(false);
-
-  async function validacaoEmail(e) {
-    const { value, name } = e.target;
-    if (value) {
-      setCamposVazios({ ...camposVazios, [name]: false });
-    } else {
-      setCamposVazios({ ...camposVazios, [name]: true });
-    }
-
-    const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (!regEx.test(value)) {
-      setErro({ ...erro, [name]: true });
-    } else {
-      setErro({ ...erro, [name]: false });
-    }
-
-    setEmail(value);
-  }
-
-  async function validacaoSenha(e) {
-    const { value, name } = e.target;
-
-    if (value) {
-      setCamposVazios({ ...camposVazios, [name]: false });
-    } else {
-      setCamposVazios({ ...camposVazios, [name]: true });
-    }
-
-    if (name === "senha" && value.length < 8) {
-      setErro({ ...erro, [name]: true });
-    } else {
-      setErro({ ...erro, [name]: false });
-    }
-
-    setSenha(value);
-  }
-
-  async function logar() {
-    setDeuUploadInicial(true);
-    if (
-      _.isEqual(camposVazios, referenciaCamposNulos) &&
-      _.isEqual(erro, referenciaCamposNulos)
-    ) {
-      login({ email, senha });
-    } else if (!_.isEqual(camposVazios, referenciaCamposNulos)) {
-      toast.error("Preencha todos os campos");
-      return;
-    } else {
-      toast.error("Preencha todos os campos corretamente");
-      return;
-    }
+  async function logar(dados) {
+    login(dados);
   }
 
   const [inputs] = useState([
@@ -129,104 +47,7 @@ function Login() {
 
   return (
     <Body>
-      <Conteudo>
-        <CaixaFoto></CaixaFoto>
-        <CaixaInput>
-          <InputNovo>
-            <TituloIcon>
-              <TituloInput>E-mail</TituloInput>
-              <MailOutlined
-                style={{ fontSize: "18px", color: "#570B87", marginTop: "10%" }}
-              />
-            </TituloIcon>
-            <Input
-              placeholder="Digite seu e-mail"
-              backgroundColor="white"
-              type="email"
-              width="50%"
-              height="100%"
-              minHeight="45px"
-              maxHeight="40px"
-              paddingRight="2%"
-              marginBottom="0%"
-              name="email"
-              value={email}
-              onChange={validacaoEmail}
-              camposVazios={camposVazios.email}
-              erro={
-                erro.email ||
-                erroLoginInvalido.email ||
-                (deuUploadInicial && camposVazios.email)
-              }
-            ></Input>
-            {erro.email && (
-              <Rotulo>Digite um email no formato email@email.com</Rotulo>
-            )}
-          </InputNovo>
-
-          <TituloIcon>
-            <TituloInput>Senha</TituloInput>
-            <LockOutlined
-              style={{ fontSize: "18px", color: "#570B87", marginTop: "10%" }}
-            />
-          </TituloIcon>
-          <Input
-            placeholder="Digite sua senha"
-            backgroundColor="white"
-            width="50%"
-            type="password"
-            height="100%"
-            minHeight="45px"
-            maxHeight="40px"
-            marginTop="0%"
-            paddingRight="2%"
-            name="senha"
-            value={senha}
-            onChange={validacaoSenha}
-            camposVazios={camposVazios.senha}
-            erro={
-              erro.senha ||
-              erroLoginInvalido.senha ||
-              (deuUploadInicial && camposVazios.senha)
-            }
-          ></Input>
-          {erro.senha && (
-            <RotuloSenha>Digite uma senha com no minimo 8 digitos</RotuloSenha>
-          )}
-        </CaixaInput>
-        <CaixaBotoes>
-          <BotoesEdicao>
-            <Botao
-              type="submit"
-              fontSize="1.2em"
-              width="40%"
-              widthMedia500="80%"
-              widthMedia280="70%"
-              onClick={() => logar()}
-            >
-              {carregando ? (
-                <Spin
-                  indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
-                />
-              ) : (
-                "Confirmar"
-              )}
-            </Botao>
-          </BotoesEdicao>
-          <BotaoCadastro
-            textDecoration="underline"
-            width="40%"
-            fontSize="1.2em"
-            widthMedia500="80%"
-            widthMedia280="80%"
-            onClick={() => {
-              window.location.href = "/Cadastro";
-            }}
-          >
-            Cadastre-se
-          </BotaoCadastro>
-        </CaixaBotoes>
-      </Conteudo>
+      <CaixaFoto />
 
       <Form
         inputs={inputs}
@@ -236,6 +57,18 @@ function Login() {
         loading={carregando}
         selectedOptionsInitial={{}}
       />
+      <BotaoCadastro
+        textDecoration="underline"
+        width="40%"
+        fontSize="1.2em"
+        widthMedia500="80%"
+        widthMedia280="80%"
+        onClick={() => {
+          window.location.href = "/Cadastro";
+        }}
+      >
+        Cadastre-se
+      </BotaoCadastro>
     </Body>
   );
 }
